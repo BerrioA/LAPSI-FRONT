2690cb16ed237f7606c9b67d3dd274ef06b02e17
BerrioA <ingalejandroberrio@gmail.com>
2025-06-30 00:44:22 -0500

    Correccion de sidebarItem

--- src/components/Sidebar/SidebarItem.jsx
+++ src/components/Sidebar/SidebarItem.jsx
@@ -2,29 +2,30 @@ import { memo } from "react";
 import { Link } from "react-router-dom";
 
 export const SidebarItem = memo(
-  ({ items, isActive,  icons, to }) => {
+  ({ items, isActive, icons, to }) => {
     const activeClass =
       "bg-primary transition-all text-off-white shadow-xl transition-colors duration-500";
     const inactiveClass =
       "text-seconndary hover:bg-light-gray hover:text-primary hover:shadow-md hover:transition-all hover:transition hover:duration-400";
     const Icon = icons;
+
     return (
-      <Link to={to}>
-        <li
+      <Link
+        to={to}
+        className="block"
+      >
+        <div
           className={`px-4 py-2 flex items-center gap-2 font-semibold rounded-lg cursor-pointer ${
             isActive ? activeClass : inactiveClass
           }`}
         >
           <Icon className="h-5 w-5" />
           {items}
-        </li>
+        </div>
       </Link>
     );
   },
-  (prevProps, nextProps) => {
-    return (
-      prevProps.isActive === nextProps.isActive &&
-      prevProps.items === nextProps.items
-    );
-  }
+  (prevProps, nextProps) =>
+    prevProps.isActive === nextProps.isActive &&
+    prevProps.items === nextProps.items
 );
